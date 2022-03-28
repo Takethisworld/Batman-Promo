88fc1f77d15a6ae0df2d1cb29172788a14a183ed
Takethisworld <saidov199102@icloud.com>
2022-03-28 15:21:52 +0500

    Adding background audio, and some animation for pagination arrow

--- script/swiper.js
+++ script/swiper.js
@@ -1,3 +1,24 @@
+const videos = document.querySelectorAll('video');
+const audio = new Audio('audio/waterTower.mp3');
+const mute = document.querySelector('.mute__checkbox');
+const pagination = document.querySelector('.pagination');
+const paginationArrow = document.querySelector('.pagination__arrow');
+
+
+try{
+    const checkMute=()=>{
+        if(mute.checked){
+            audio.play().catch(()=>{
+                mute.checked=false;
+            });
+        }else{
+            audio.pause()
+        }
+    };
+    if(mute){
+        setTimeout(checkMute,2000)
+    }
+
 const sliderThumbs = new Swiper('.slider-thumbs',{
     loop:true,
     spaceBetween:20,
@@ -15,5 +36,18 @@ const sliderMain = new Swiper('.slider-main',{
     loopedSlides:6
 })
 
+sliderMain.on('slideChange',()=>{
+    for(let i=0;i<videos.length; i+=1){
+        videos[i].pause();
+    }
+} )
+
+paginationArrow.addEventListener('click',()=>{
+    pagination.classList.toggle('pagination_active');
+})
+
 sliderThumbs.controller.control = sliderMain;
 sliderMain.controller.control = sliderThumbs;
+}catch{
+    console.log('That slide doesnt work')
+}
